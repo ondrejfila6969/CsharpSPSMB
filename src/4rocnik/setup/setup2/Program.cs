63807b3b15e41a3633c2732a6e3dc57c66d1e640
ondrejfila6969 <ofila12@seznam.cz>
2025-09-04 11:01:30 +0200

    setup

---
+++ src/4rocnik/setup/setup2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+    } else if (i % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    } else if (i % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+void fizzbuzz(int number)
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+    } else if (number % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    } else if (number % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else
+    {
+        Console.WriteLine(number);
+    }
+}
+
+fizzbuzz(15);
