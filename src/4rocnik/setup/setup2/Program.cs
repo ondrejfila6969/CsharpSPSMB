6a5681b535fb861b87e23bce333d7697805d5137
ondrejfila6969 <ofila12@seznam.cz>
2025-09-04 12:18:47 +0200

    1) maturita practice

--- src/4rocnik/setup/setup2/Program.cs
+++ src/4rocnik/setup/setup2/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// FIZZBUZZ
+/*
 for (int i = 1; i <= 100; i++)
 {
     if (i % 3 == 0 && i % 5 == 0)
@@ -37,3 +39,45 @@ void fizzbuzz(int number)
 }
 
 fizzbuzz(15);
+*/
+
+// HÁDÁNÍ ČÍSLA
+bool programRunning = true;
+
+Console.WriteLine("Zadejte horní hranici: ");
+int maxValue = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Zadejte spodní hranici: ");
+int minValue = Int32.Parse(Console.ReadLine());
+
+var random = new Random();
+int programNumber = random.Next(minValue, maxValue);
+Console.WriteLine(programNumber);
+
+int tries = 0;
+do
+{
+    Console.WriteLine("Zadejte váš odhad: ");
+    int guess = Int32.Parse(Console.ReadLine());
+    
+    if (guess != programNumber)
+    {
+        if (guess > programNumber)
+        {
+            Console.WriteLine("Číslo, které jste typoval, je větší než číslo programu");
+            tries++;
+        }
+        else if (guess < programNumber)
+        {
+            Console.WriteLine("Číslo, které jste typoval, je menší než číslo programu");
+            tries++;
+        }
+    } else
+    {
+        tries++;
+        Console.WriteLine($"Gratulujeme, uhádl jste číslo a váš počet pokusů: {tries}");
+        programRunning = false;
+    }
+} while (programRunning);
+
+
